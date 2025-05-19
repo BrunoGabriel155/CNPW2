c527501bf50f07231d1307588931cbb9021f2b41
Bruno Gabriel <147466021+BrunoGabriel155@users.noreply.github.com>
2025-05-19 10:51:03 -0300

    Add files via upload

---
+++ Portifolio_de_Imagens_19-05/cnpw2/Aula11/imagemParaBucket.ps1
@@ -0,0 +1,13 @@
+﻿# Script PowerShell para criar bucket S3 com LocalStack
+
+
+$Endpoint = "http://localhost:4566"
+$Bucket = Read-Host -Prompt "Insira o nome do bucket"
+$Pasta = Read-Host -Prompt "Insira o nome da Pasta"
+
+# 1. Criar um bucket
+Write-Host "Copiando as imagens para o Bucket..."
+aws s3 cp --recursive $Pasta "s3://$Bucket" --endpoint-url $Endpoint --profile "localstack"
+
+# 2. Lista as imagens copiadas para o Bucket
+aws s3 ls "s3://$Bucket/$Pasta" --endpoint-url $Endpoint --profile "localstack"
